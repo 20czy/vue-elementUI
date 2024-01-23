fa2aba5209b5d50d4ea61880a00385c028817eb8
20czy <3092492683@qq.com>
2024-01-23 16:36:51 +0800

    first time successfully connect to the backEnd,wuho!

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,48 +1,46 @@
 <template>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-  </el-table>
+  <div class="center">
+  <el-table :data="tableData" border style="width: 60%">
+  <el-table-column prop="sid" label="id"></el-table-column>
+  <el-table-column prop="name" label="姓名"></el-table-column>
+  <el-table-column prop="age" label="年龄"></el-table-column>
+  <el-table-column prop="birthday" label="生日"></el-table-column>
+</el-table>
+
+  </div>
 </template>
 
 <script>
+import axios from 'axios';
+
   export default {
     name: 'HelloWorld',
     data() {
       return {
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
+        tableData: [],
+      };
+    },
+    created:function () {
+      this.fetchDate();
+    },
+    methods: {
+      fetchDate() {
+        axios.get("http://localhost:8081/User").then((respond) => {
+          this.tableData = respond.data
+        }).catch((error) => {console.error('Error fetching data', error)})
       }
+    },
+    mounted:function () {
+      console.log("app was mounted")
+    },
     }
-  }
 </script>
+
+<style scoped>
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
